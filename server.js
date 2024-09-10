95988320fccd2d9b16926e630870c4c7b6c3b59b
nathaliafreboucas <contato.nathaliareboucas@outlook.com>
2024-09-10 15:36:30 -0300

    corrigindo api

--- server.js
+++ server.js
@@ -4,10 +4,17 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
-const port = process.env.PORT || 8080;
 
 server.use(middlewares);
+server.use(
+    jsonServer.rewriter({
+        "/*":"/$1",
+    })
+);
+
 server.use(router);
-server.listen(port, () => {
+server.listen(3000 || 8080, () => {
     console.log(`JSON Server is running in ${port}`);
 })
+
+module.exports = server;
